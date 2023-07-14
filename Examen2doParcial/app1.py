c389a749608d1c499cf3cab2f5e30697451a5916
YaritziLeon <yaritzileonm08@gmail.com>
2023-07-14 17:11:45 -0600

    SegundoCommit ExamenParcial

--- Examen2doParcial/app1.py
+++ Examen2doParcial/app1.py
@@ -10,7 +10,15 @@ app.config['MYSQL_DB']='DB_Floreria'
 app.secret_key='mysecretkey'
 mysql=MySQL(app)
 
-app.route('/ingresar', methods=['POST'])
+@app.route('/')
+def index1():
+    CC=mysql.connection.cursor()
+    CC.execute('select * from tbflores')
+    Flores=CC.fetchall()
+    return render_template('index1.html',listaFlores=Flores)
+    
+
+@app.route('/ingresar', methods=['POST'])
 def ingresar():
     if request.method =='POST':
         Vnombre=request.form['txtNombre']
@@ -20,7 +28,13 @@ def ingresar():
         CS.execute('insert into tbflores (nombre,cantidad,precio) values (%s,%s,%s)',(Vnombre,Vcantidad,Vprecio))
         mysql.connection.commit()
         
+        flash('La flor fue agregada')
+        return redirect(url_for('index1'))
+
+@app.route('/eliminar/<id>',methods=['POST'])
+    def eliminar(id):
+        if request.method== 'POST':
+    
 
 if __name__ == '__main__':
-    app.run(port=5001)
     app.run(port=5001,debug=True)
